0df49c0c249826c1a0e46bb330aa67f81ed33c7d
The Mini John <admin@phoenix-prime.gr>
2013-09-10 21:34:30 +0300

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Amphy::Application.config.secret_key_base = 'dc8af94ccece3e97c98776ab2cbeaa11f162e5f2d8cd68c73005f764cc7a798b262939f957f6b0a44bbf6325a5ffe7ecadaa2b35fb8113fcf6d00a7900126d3a'
